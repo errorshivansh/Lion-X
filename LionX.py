9b8ab4b27ea198e8e8be9464c4f1cf890d4eb3f1
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-07-11 08:40:18 +0530

    Add files via upload

---
+++ LionX.py
@@ -0,0 +1,6 @@
+from os import system
+system("git clone https://gitHub.com/TeamLionX/Lion-X && cd Lion-X && python3 -m LIONX")
+# if got error
+print(" Sσʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
+# Script written for stop heroku suspension
+# use with credit else gay
